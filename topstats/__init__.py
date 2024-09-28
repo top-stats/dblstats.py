e4e059559aa6f6857e43de4ffb8fc7390a22b718
null8626 <vierofernando9@gmail.com>
2024-09-28 19:51:21 +0700

    meta: add __copyright__

--- topstats/__init__.py
+++ topstats/__init__.py
@@ -28,5 +28,6 @@ from .client import Client
 __title__ = 'topstats'
 __author__ = 'null8626'
 __license__ = 'MIT'
+__copyright__ = 'Copyright (c) 2020 Arthurdw; Copyright (c) 2024 null8626'
 __version__ = '1.0.0'
 __all__ = ('Client',)
